2b61a5ada0b42babd9d25591b66e6940dfbe89b3
kimbyeongnyeon <qud5252@naver.com>
2026-05-03 21:43:41 +0900

    feat: 대시보드 도메인 용 유틸함수 추가

---
+++ app/domain/dashboard/utils/dashboard.utils.ts
@@ -0,0 +1,53 @@
+import { Activity } from "../../activities/types";
+import {
+  dashboardCategoryColors,
+  dashboardCategoryLabels,
+  targetEmissionValue,
+} from "../constants/dashboard.constants";
+import { DashboardCategoryItem, DashboardMonthlyItem } from "../types";
+
+export const formatEmissionNumber = (value: number) => {
+  return value.toLocaleString("ko-KR", {
+    maximumFractionDigits: 2,
+    minimumFractionDigits: 0,
+  });
+};
+
+export const formatDashboardDate = (date: string) => {
+  return new Intl.DateTimeFormat("ko-KR", {
+    month: "2-digit",
+    day: "2-digit",
+  })
+    .format(new Date(date))
+    .replaceAll(". ", "-")
+    .replace(".", "");
+};
+
+export const getCategoryRatioData = (
+  categorySummary: DashboardCategoryItem[],
+) => {
+  return categorySummary.map((item) => ({
+    color: dashboardCategoryColors[item.category],
+    name: dashboardCategoryLabels[item.category],
+    value: item.emissionValue,
+  }));
+};
+
+export const getMonthlyTrendData = (monthlyTrend: DashboardMonthlyItem[]) => {
+  return monthlyTrend.map((item) => ({
+    month: item.month.slice(2).replace("-", "."),
+    emissionValue: item.emissionValue,
+  }));
+};
+
+export const getTargetProgress = (totalEmission: number) => {
+  return Math.round((totalEmission / targetEmissionValue) * 1000) / 10;
+};
+
+export const getRecentActivitiesForTable = (activities: Activity[]) => {
+  return activities.map((activity) => ({
+    ...activity,
+    displayDate: formatDashboardDate(activity.activityDate),
+    displayCategory: dashboardCategoryLabels[activity.category],
+  }));
+};
